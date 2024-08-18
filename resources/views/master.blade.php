9dabe4468c2309bfc533eafb3fe8d618f48ebc9a
jefkruz <josephugbeva@gmail.com>
2024-08-18 14:01:20 +0100

    helo

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -239,7 +239,7 @@
                     <div class="portfolio-item img-zoom ct-photography ct-media ct-branding ct-Media">
                         <div class="portfolio-item-wrap">
                             <div class="portfolio-image">
-                                <a href="#"><img src="{{$image->url}}" style="max-height: 200px" alt=""></a>
+                                <a href="#"><img src="{{$image->url}}" alt=""></a>
                             </div>
                             <div class="portfolio-description">
                                 <a title="Paper Pouch!" data-lightbox="gallery-image" href="{{$image->url}}" class="btn btn-light btn-roundeded">Zoom</a>
